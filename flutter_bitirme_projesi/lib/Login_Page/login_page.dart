64b34ac98ec55fd459d184d2d80650b65228b181
Ahmet Yildiz <ahmet91642@gmail.com>
2024-01-12 20:55:54 +0300

    degisti

---
+++ flutter_bitirme_projesi/lib/Login_Page/login_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bitirme_projesi/Use_General_Project/custom_text_widget.dart';
+import 'package:flutter_bitirme_projesi/Use_General_Project/general_frame.dart';
+import 'package:flutter_bitirme_projesi/Use_General_Project/navigate_other_auth.dart';
+import 'package:flutter_bitirme_projesi/Use_General_Project/padding_sizes.dart';
+import 'package:flutter_bitirme_projesi/Use_General_Project/project_button.dart';
+import 'package:flutter_bitirme_projesi/Use_General_Project/project_colors.dart';
+import 'package:flutter_bitirme_projesi/Sign_In_Page/sign_in.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final String title = "Giriş Yap";
+  final String hintText1 = "T.C Numarası";
+  final String hintText2 = "Şifre";
+  final String buttonText = "Giriş Yap";
+  final String underText = "Hesabınız Yok Mu?";
+  final String underText2 = "Kayıt Ol";
+  final double textFieldWidth = 400;
+  final double textFieldHeight = 40;
+  final double fontSize = 28;
+  @override
+  Widget build(BuildContext context) {
+    return GeneralFrame(
+      child: Padding(
+        padding: PaddingSizes().innerFrame,
+        child: Container(
+          color: ProjectColors().darkTheme,
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Text(
+                    title,
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                          color: ProjectColors().background,
+                          fontSize: fontSize,
+                        ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 70),
+                  child: Column(
+                    children: [
+                      CustomTextWidget(
+                        textInputAction: TextInputAction.next,
+                        hintText: hintText1,
+                        width: textFieldWidth,
+                        height: textFieldHeight,
+                      ),
+                      CustomTextWidget(
+                        textInputAction: TextInputAction.done,
+                        hintText: hintText2,
+                        width: textFieldWidth,
+                        height: textFieldHeight,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30),
+                        child: ProjectButtonStyle(
+                            title: buttonText,
+                            fontSize: fontSize,
+                            textColor: ProjectColors().darkTheme,
+                            buttonColor: ProjectColors().likePink),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 90),
+                        child: NavigateToOtherAuth(
+                            widget: KayitOl(),
+                            title: underText,
+                            navigateText: underText2),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
