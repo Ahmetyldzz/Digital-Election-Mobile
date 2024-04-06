d55c84bf54590e4ba25fbb8ede51d36595adcc8c
Ahmet Yildiz <ahmet91642@gmail.com>
2024-04-06 16:27:16 +0300

    EnteredHomePageUpdate

--- flutter_bitirme_projesi/lib/Entered_Homepage/election_enter_type_interface.dart
+++ flutter_bitirme_projesi/lib/Entered_Homepage/election_enter_type_interface.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bitirme_projesi/Use_General_Project/navigateToPage.dart';
 import 'package:flutter_bitirme_projesi/Use_General_Project/project_colors.dart';
+import 'package:flutter_bitirme_projesi/Voting_Page/oy_kullanma.dart';
 
 class ElectionEnterTypeMenu extends StatefulWidget {
   const ElectionEnterTypeMenu({super.key});
@@ -8,8 +10,10 @@ class ElectionEnterTypeMenu extends StatefulWidget {
   State<ElectionEnterTypeMenu> createState() => _ElectionEnterTypeMenuState();
 }
 
-class _ElectionEnterTypeMenuState extends State<ElectionEnterTypeMenu> {
+class _ElectionEnterTypeMenuState extends State<ElectionEnterTypeMenu>
+    with NavigatorRoute {
   bool _ActiveButton = false;
+  TextEditingController textEditingController = TextEditingController();
   void buttonChanged() {
     setState(() {
       _ActiveButton = !_ActiveButton;
@@ -25,7 +29,7 @@ class _ElectionEnterTypeMenuState extends State<ElectionEnterTypeMenu> {
           color: ProjectColors().background,
         ),
         width: 250,
-        height: 250,
+        height: 180,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -37,112 +41,47 @@ class _ElectionEnterTypeMenuState extends State<ElectionEnterTypeMenu> {
               ),
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Expanded(
-                  child: SizedBox(
-                    width: 175,
-                    child: Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: (_ActiveButton == false)
-                                ? ProjectColors().commonTheme
-                                : ProjectColors().likePink,
-                            shape:
-                                RoundedRectangleBorder(side: BorderSide.none)),
-                        onPressed: () {
-                          _ActiveButton == false ? buttonChanged() : null;
-                        },
-                        child: Text(
-                          "ID ile giriş",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyLarge
-                              ?.copyWith(
-                                  color: _ActiveButton == false
-                                      ? ProjectColors().background
-                                      : ProjectColors().darkTheme),
-                        ),
+                  flex: 8,
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Container(
+                      color: Colors.white,
+                      height: 50,
+                      child: TextField(
+                        controller: textEditingController,
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                            hintText: "ID ile giriş ",
+                            border: OutlineInputBorder(),
+                            focusColor: Colors.white),
                       ),
                     ),
                   ),
                 ),
-                /*  Expanded(
-                  child: SizedBox(
-                    width: 175,
+                Expanded(
+                    flex: 2,
                     child: Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: (_ActiveButton == true)
-                                ? ProjectColors().commonTheme
-                                : ProjectColors().likePink,
-                            shape:
-                                RoundedRectangleBorder(side: BorderSide.none)),
-                        onPressed: () {
-                          _ActiveButton == true ? buttonChanged() : null;
+                      padding: const EdgeInsets.all(8.0),
+                      child: InkWell(
+                        onTap: () {
+                          /* if (textEditingController.text == Election.id) {
+                            navigateToWidget(context, Oykullanma());
+                          } */
                         },
-                        child: Text(
-                          "URL ile giriş",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyLarge
-                              ?.copyWith(
-                                  color: _ActiveButton == true
-                                      ? ProjectColors().background
-                                      : ProjectColors().darkTheme),
+                        child: CircleAvatar(
+                          backgroundColor: ProjectColors().commonTheme,
+                          child: Icon(
+                            Icons.chevron_right_rounded,
+                            color: ProjectColors().background,
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                ), */
+                    )),
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-              child: Container(
-                color: Colors.white,
-                height: 30,
-                child: TextField(
-                  keyboardType: (_ActiveButton == true)
-                      ? TextInputType.phone
-                      : TextInputType.url,
-                  decoration: InputDecoration(
-                      hintText: (_ActiveButton == true)
-                          ? "ID ile giriş "
-                          : "URL ile giriş",
-                      border: OutlineInputBorder(),
-                      focusColor: Colors.white),
-                ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 30),
-                  child: SizedBox(
-                    width: 100,
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: ProjectColors().commonTheme,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10))),
-                        onPressed: () {},
-                        child: Text(
-                          "Seçime Bağlanın",
-                          style: Theme.of(context)
-                              .textTheme
-                              .displaySmall
-                              ?.copyWith(
-                                  fontSize: 14,
-                                  color: ProjectColors().background),
-                        )),
-                  ),
-                )
-              ],
-            )
           ],
         ),
       ),
